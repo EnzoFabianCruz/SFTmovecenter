e2d8a6f79252f6677e714f30996614bd3e8a56c6
EnzoFabianCruz <jewelryaccess7@gmail.com>
2025-07-11 18:54:00 -0500

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -2,16 +2,23 @@
 
 set -e
 
-apt-get update && apt-get install -y curl gnupg2 apt-transport-https software-properties-common unixodbc-dev
+# Instala dependencias necesarias
+apt-get update && apt-get install -y \
+    curl \
+    gnupg2 \
+    apt-transport-https \
+    software-properties-common \
+    unixodbc-dev \
+    gcc \
+    g++ \
+    make
 
-# Repositorio de Microsoft para ODBC
+# Agrega el repositorio de Microsoft
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
+# Instala el driver ODBC 17
 apt-get update && ACCEPT_EULA=Y apt-get install -y msodbcsql17
 
-# Instalar las dependencias Python
+# Instala dependencias de Python
 pip install -r requirements.txt
-
-# Verificar que el driver fue instalado
-odbcinst -q -d -n "ODBC Driver 17 for SQL Server"
