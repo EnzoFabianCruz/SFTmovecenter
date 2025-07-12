57f027d397c30842feb9c5b554cde0d22297e636
EnzoFabianCruz <jewelryaccess7@gmail.com>
2025-07-11 19:50:10 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
-# Instala dependencias
+# Instala dependencias base
 apt-get update && apt-get install -y \
     curl \
     gnupg2 \
     apt-transport-https \
     software-properties-common \
-    unixodbc-dev \
     gcc \
     g++ \
     make
 
-# Agrega repositorio de Microsoft
+# Agrega la clave GPG y el repositorio de Microsoft
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
-# Actualiza e instala driver ODBC 18
-apt-get update && ACCEPT_EULA=Y apt-get install -y msodbcsql18
+# Actualiza los repos y asegura instalación limpia de unixODBC y msodbcsql18
+apt-get update
 
-# Instala dependencias de Python
+# Instala msodbcsql18 y unixODBC (el orden es importante para evitar conflictos)
+ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev
+
+# Verifica que el driver ODBC esté instalado correctamente
+echo "Drivers ODBC instalados:"
+odbcinst -q -d | grep -i "ODBC Driver"
+
+# Instala pip y dependencias de Python
+pip install --upgrade pip
 pip install -r requirements.txt
