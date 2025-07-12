4c4e0e2bf6d8efacd61ce4f6e3121d73adedc2a0
EnzoFabianCruz <jewelryaccess7@gmail.com>
2025-07-11 19:10:49 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# Instala dependencias necesarias
+# Actualiza e instala herramientas necesarias
 apt-get update && apt-get install -y \
     curl \
     gnupg2 \
@@ -11,14 +11,19 @@ apt-get update && apt-get install -y \
     unixodbc-dev \
     gcc \
     g++ \
-    make
+    make \
+    python3-dev \
+    python3-pip
 
-# Agrega el repositorio de Microsoft
+# Agrega repositorio de Microsoft ODBC
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
 # Instala el driver ODBC 17
 apt-get update && ACCEPT_EULA=Y apt-get install -y msodbcsql17
 
-# Instala dependencias de Python
+# Actualiza pip y wheel antes de instalar dependencias
+python3 -m pip install --upgrade pip wheel setuptools
+
+# Instala las dependencias del proyecto
 pip install -r requirements.txt
